9dd9191e47ea4ceb7398fd11dc7dc97e75d11de6
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-02-02 22:35:42 +0900

    feat: improved game config

--- src/main/java/net/kunmc/lab/toiletplugin/game/GameConfig.java
+++ src/main/java/net/kunmc/lab/toiletplugin/game/GameConfig.java
@@ -4,15 +4,9 @@ import net.kunmc.lab.toiletplugin.game.config.Config;
 
 public class GameConfig
 {
-    @Config(helpMessage = "テスト。")
-    private String test;
+    @Config(min = 1, helpMessage = "クエストが始まってから死ぬまでの秒数範囲：最小")
+    public int minQuestProgressTime = 20;
+    @Config(min = 1, helpMessage = "クエストが始まってから死ぬまでの秒数範囲：最大")
+    public int maxQuestProgressTime = 30;
 
-    @Config(toggle = true)
-    private boolean hoge;
-
-    @Config(min = 20, max = 25)
-    private int fuga;
-
-    @Config(name = "testB")
-    private String hogera;
 }
