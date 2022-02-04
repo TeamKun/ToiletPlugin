8e516175476b9a0e2c775d890bc4071d0a1ac4c0
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-02-05 04:12:52 +0900

    feat: Purge state

--- src/main/java/net/kunmc/lab/toiletplugin/game/toilet/OnGroundToilet.java
+++ src/main/java/net/kunmc/lab/toiletplugin/game/toilet/OnGroundToilet.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import net.kunmc.lab.toiletplugin.toiletobject.Toilet;
 import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.type.Door;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -45,6 +47,29 @@ public class OnGroundToilet extends Toilet
     public void killEntities()
     {
         displays.forEach(Entity::remove);
+    }
+
+    public void setState(ToiletState state)
+    {
+        this.state = state;
+        if (state == ToiletState.OPEN)
+            this.purge();
+    }
+
+    public void purge()
+    {
+        this.state = ToiletState.OPEN;
+        this.timesElapsed = 0;
+        this.cooldownMax = 0;
+        this.toiletPlayer = null;
+
+        Block doorBlock = this.getDoorLocation().toLocation().getBlock();
+
+        if (!(doorBlock.getBlockData() instanceof Door))
+            return;
+        Door door = (Door) doorBlock.getBlockData();
+        door.setOpen(true);
+        doorBlock.setBlockData(door, true);
     }
 
 }
