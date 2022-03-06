195b0c5743a8b40768a9d4531e6d62898e6e6b77
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-03-07 02:02:05 +0900

    fix: Help subcommand

--- src/main/java/net/kunmc/lab/toiletplugin/commands/HelpCommand.java
+++ src/main/java/net/kunmc/lab/toiletplugin/commands/HelpCommand.java
@@ -34,10 +34,23 @@ public class HelpCommand extends CommandBase
                 .collect(Collectors.toList());
     }
 
+    private static void send(CommandSender sender, TextComponent helpMessage, String commandName, int space_size, String subCommand)
+    {
+        sender.sendMessage(suggestCommand(
+                of(ChatColor.AQUA + "/toilet " + (subCommand != null ? subCommand + " ": "")
+                        + commandName + " " + StringUtils.repeat(" ", space_size - commandName.length()) + " - ")
+                        .append(Component.text(ChatColor.DARK_AQUA.toString()))
+                        .append(helpMessage.color(NamedTextColor.DARK_AQUA)),
+                "/toilet " + (subCommand != null ? subCommand + " ": "") + commandName
+        ));
+    }
+
     @Override
     public void onCommand(CommandSender sender, String[] args)
     {
         Map<String, CommandBase> commands = this.commands;
+        boolean subCommand = false;
+
         if (args.length > 0 && commands.containsKey(args[0]))
         {
             CommandBase command = commands.get(args[0]);
@@ -45,19 +58,21 @@ public class HelpCommand extends CommandBase
             {
                 SubCommandable subCommandable = (SubCommandable) command;
                 commands = subCommandable.getSubCommands();
+                subCommand = true;
             }
         }
 
-
         int page_max = commands.size() / 5 + 1;
+
 
         int page = 1;
         try
         {
-            if (args.length > 2)
+            if (args.length > 1)
                 page = Integer.parseInt(args[1]);
             else if (args.length > 0)
-                page = Integer.parseInt(args[0]);
+                if (args[0].matches("[0-9]+"))
+                    page = Integer.parseInt(args[0]);
         }
         catch (NumberFormatException e)
         {
@@ -72,19 +87,21 @@ public class HelpCommand extends CommandBase
 
         int max_length = commands.keySet().stream().mapToInt(String::length).max().orElse(0);
 
+        boolean finalSubCommand = subCommand;
         commands.entrySet().stream()
                 .skip(start)
                 .limit(end - start)
                 .forEach(entry -> send(sender, entry.getValue().getHelpOneLine().append(
-                                of("\n    " + String.join(" ", entry.getValue().getArguments()))),
-                        entry.getKey(), max_length
-                ));
+                                        of("\n    " + String.join(" ", entry.getValue().getArguments()))),
+                                entry.getKey(), max_length, (finalSubCommand ? args[0]: null)
+                        )
+                );
 
         TextComponent footer = of(ChatColor.GOLD + "-----=====");
 
         if (page > 1)
             footer = footer.append(of(ChatColor.GOLD + " [" + ChatColor.RED + "<<" + ChatColor.GOLD + "]")
-                    .clickEvent(ClickEvent.runCommand("/toilet help " + (page - 1)))
+                    .clickEvent(ClickEvent.runCommand("/toilet help " + (subCommand ? args[0] + " ": "") + (page - 1)))
                     .hoverEvent(HoverEvent.showText(of(ChatColor.AQUA + "クリックして前のページに戻る"))));
         else
             footer = footer.append(of("     "));
@@ -92,22 +109,13 @@ public class HelpCommand extends CommandBase
         footer = footer.append(of(ChatColor.GOLD + " ToiletPlugin "));
 
         if (page < page_max)
-            footer = footer.append(of( ChatColor.GOLD + "[" + ChatColor.GREEN + ">>" + ChatColor.GOLD + "] ")
-                    .clickEvent(ClickEvent.runCommand("/toilet help " + (page + 1)))
+            footer = footer.append(of(ChatColor.GOLD + "[" + ChatColor.GREEN + ">>" + ChatColor.GOLD + "] ")
+                    .clickEvent(ClickEvent.runCommand("/toilet help " + (subCommand ? args[0] + " ": "") + (page + 1)))
                     .hoverEvent(HoverEvent.showText(of(ChatColor.AQUA + "クリックして次のページに進む"))));
         else
             footer = footer.append(of("    "));
 
         sender.sendMessage(footer.append(of(ChatColor.GOLD + "=====-----")));
-    }
-
-    private static void send(CommandSender sender,  TextComponent helpMessage, String commandName, int space_size)
-    {
-        sender.sendMessage(suggestCommand(of(ChatColor.AQUA + "/toilet " + commandName + " " +
-                        StringUtils.repeat(" ", space_size - commandName.length()) + " - ")
-                .append(Component.text(ChatColor.DARK_AQUA.toString()))
-                .append(helpMessage.color(NamedTextColor.DARK_AQUA)),
-                "/toilet " + commandName));
     }
 
     @Override
